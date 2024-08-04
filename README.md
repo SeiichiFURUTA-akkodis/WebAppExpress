19540c37d6173ff69fdde878e71567e192e8e003
Seiichi FURUTA <seiichi.furuta@akkodis.co.jp>
2024-08-04 04:04:35 +0000

    Expressを新規作成、SQLフォルダ追加

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WebAppExpress
 「たけうち百貨店」をNode.js（express）ベースにシフトする
+npm install -g express-generator
+express --view=ejs backend
+cd backend
+npm install
+npm start
---
+++ backend/sql/utf8/postgres/05_INSERT_M_CATEGORY.sql
@@ -0,0 +1,7 @@
+insert into M_CATEGORY ( CATID, CATNAME ) values (1001, 'ビール');
+insert into M_CATEGORY ( CATID, CATNAME ) values (1002, '日本酒');
+insert into M_CATEGORY ( CATID, CATNAME ) values (1003, '泡盛');
+insert into M_CATEGORY ( CATID, CATNAME ) values (2001, 'お菓子');
+insert into M_CATEGORY ( CATID, CATNAME ) values (3001, 'インナー');
+insert into M_CATEGORY ( CATID, CATNAME ) values (3002, 'アウター');
+insert into M_CATEGORY ( CATID, CATNAME ) values (3003, 'オールインワン');
